fabe005844f3941568585571728ab90e9411a990
Jarad Touchberry <you@example.com>
2017-05-01 12:44:07 -0400

    webbrowser

---
+++ src/sample/Controller.java
@@ -0,0 +1,66 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, ChangeListener {
+    @FXML
+    TextField addressBar;
+
+    @FXML
+    WebView webView;
+
+    public void goBack() {
+        try {
+            webView.getEngine().getHistory().go(-1);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void goForward() {
+        try {
+            webView.getEngine().getHistory().go(1);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void goToUrl() {
+        String url = addressBar.getText();
+
+        if (!url.startsWith("http")) {
+            url = "http://" + url;
+        }
+
+        webView.getEngine().load(url);
+    }
+
+    public void onKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            goToUrl();
+        }
+    }
+
+    //ties the web view together with the change even listener
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Worker worker = webView.getEngine().getLoadWorker();
+        worker.stateProperty().addListener(this);
+    }
+    //will be invoked when you change urls in the web view
+    @Override
+    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+        addressBar.setText(webView.getEngine().getLocation());
+    }
+}
